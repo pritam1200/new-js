a8eb94217078717053e9d2637460d02a77ce6c84
Pritam Gharami <pritamgharami1200@gmail.com>
2023-10-18 14:23:29 +0000

    dataTypes

---
+++ js_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)  //we are using node.js, not browser
+
+
+console.log(3
+    +3) // code readability should be high
+console.log("Pritam")
+
+
+let name ="pritam"
+let age =25
+let isLoggedIn = false
+
+//number => 2 ot power 53
+// bigint
+// string => true
+// boolean => true/false
+// null=> stand alone value
+// undefined
+// symbol => unique
+
+console.log(typeof age);
