dfd7bd4960f330f0db4dcc01681978b2048a010e
Pritam Gharami <pritamgharami1200@gmail.com>
2023-11-01 17:15:43 +0000

    prefixAndProfix

--- js_basics/03_dataConvertion.js
+++ js_basics/03_dataConvertion.js
@@ -1,19 +1,44 @@
-/* let value= 3
-let negValue = -value
-console.log(negValue);
-console.log(typeof(negValue))
-console.log(2**3)*/ // 2 the power of 3  "**" <-- is mean power
 
-let isLoggedIn ="pritam"
+// let isLoggedIn ="pritam"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn)
 
-// 1 => true; 0 => false
-// "" => true
-// "pritam" => true
+// // 1 => true; 0 => false
+// // "" => true
+// // "pritam" => true
 
-let someNumber= 33
-let stringNumber = String(someNumber)
-console.log(typeof(stringNumber));
-console.log(stringNumber);
+// let someNumber= 33
+// let stringNumber = String(someNumber)
+// console.log(typeof(stringNumber));
+// console.log(stringNumber);
+
+// ************************** Operations *********************************
+//  let value= 3
+// let negValue = -value
+// console.log(negValue);
+// console.log(typeof(negValue))
+// console.log(2**3) // 2 the power of 3  "**" <-- is mean power
+
+// let str1 ="hello"
+// let str2 = " pritam"
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2+2);
+// console.log(1+2+"pritam");
+
+console.log(+true);
+console.log(+"");
+let num1, num2, num3
+
+num1= num2 =num3 =2+2
+
+// let gameCounter =100
+// gameCounter++;
+// console.log(gameCounter);
+
+
+let gameCounter =100
+++gameCounter;
+console.log(gameCounter);
