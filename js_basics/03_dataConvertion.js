222057baf643bb81ba24b7072c566cf56b25825b
Pritam Gharami <pritamgharami1200@gmail.com>
2023-11-01 16:59:44 +0000

    convertionOfDataType

--- js_basics/03_dataConvertion.js
+++ js_basics/03_dataConvertion.js
@@ -4,4 +4,16 @@ console.log(negValue);
 console.log(typeof(negValue))
 console.log(2**3)*/ // 2 the power of 3  "**" <-- is mean power
 
+let isLoggedIn ="pritam"
 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => true
+// "pritam" => true
+
+let someNumber= 33
+let stringNumber = String(someNumber)
+console.log(typeof(stringNumber));
+console.log(stringNumber);
