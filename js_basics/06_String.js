8d3d6880ff08a1d9e2d3b6fe2493a0dd5caefb78
Pritam Gharami <pritamgharami1200@gmail.com>
2023-11-14 11:39:48 +0000

    strings

---
+++ js_basics/06_String.js
@@ -0,0 +1,22 @@
+// const name = "Pritam" 
+// const repoCount = 22
+
+// // console.log(name + repoCount +"value");   old method of concadination
+
+
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('PritamG') // another way of declearing string
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt('5'));
+console.log(gameName.indexOf('t'));
+
+
+
