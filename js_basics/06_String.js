5efdbdbac52445cb17a278fc6638e2c6214a1d5c
Pritam Gharami <pritamgharami1200@gmail.com>
2023-11-14 18:17:07 +0000

    strings

--- js_basics/06_String.js
+++ js_basics/06_String.js
@@ -7,7 +7,7 @@
 
 // console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('PritamG') // another way of declearing string
+const gameName = new String('Pritam-G-com') // another way of declearing string
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -17,6 +17,26 @@ console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt('5'));
 console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(2,5)
+console.log(newString);
+
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+
+const newStringOne = "   Pritam   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20','-'));
+
+
+console.log(url.includes('sunder'));
+
+console.log(gameName.split('-'));
 
 
 
