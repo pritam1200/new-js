068ec19cfaaf68113d036acbb001ebd6c4d75ecd
Pritam Gharami <pritamgharami1200@gmail.com>
2023-11-14 10:08:21 +0000

    data types

--- js_basics/05_dataTypes-summery.js
+++ js_basics/05_dataTypes-summery.js
@@ -1,1 +1,18 @@
 // Premitive
+
+// 7 types : String, Number, Bolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn =false
+const outsideTEmp = null
+let userEmail; // or let outsideTemp = undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
