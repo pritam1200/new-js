3123a8712ed90ff40048b9c932e32da02f421291
Pritam Gharami <pritamgharami1200@gmail.com>
2023-11-14 10:56:31 +0000

    data types

--- js_basics/05_dataTypes-summery.js
+++ js_basics/05_dataTypes-summery.js
@@ -6,13 +6,28 @@ const score = 100
 const scoreValue = 100.3
 
 const isLoggedIn =false
-const outsideTEmp = null
+const outsideTemp = null
 let userEmail; // or let outsideTemp = undefined
 
 const id = Symbol('123')
 const anotherId = Symbol('123')
+
 console.log(id === anotherId);
+
+const bigNumber = 846513213484654865136846513n //this is bigInt
 
 // Reference (Non primitive)
 
 // Array, Objects, Functions
+
+const hero = ["Pritam", "Swapna", "Sreeja"];
+let myObj={
+    name: "Pritam",
+    age: 22,
+}
+
+const myFunction= function(){
+    console.log("Hello owrld");
+
+}
+console.log(typeof myObj);
