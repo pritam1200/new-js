abcb6790721d0b5f25bd2886b56b5dc5f989a296
Pritam Gharami <pritamgharami1200@gmail.com>
2023-11-16 09:59:11 +0000

    date in JavaScript

---
+++ js_basics/08_datesInJs.js
@@ -0,0 +1,8 @@
+// Dates 
+
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
