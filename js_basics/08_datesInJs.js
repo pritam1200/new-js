f33c672ffa48e52b9e0a25825605070270b152e7
Pritam Gharami <pritamgharami1200@gmail.com>
2023-11-16 11:04:35 +0000

    Date and Time

--- js_basics/08_datesInJs.js
+++ js_basics/08_datesInJs.js
@@ -2,7 +2,31 @@
 
 
 let myDate = new Date()
-console.log(myDate.toString());
-console.log(myDate.toLocaleString());
-console.log(myDate.toLocaleDateString());
-console.log(typeof myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3, 51)
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(myCreatedDate.getDay());
+// console.log(myCreatedDate.getDate());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+// console.log(`${newDate.getDay()} and the time is ${Math.floor(newDate.getTime()/1000)}`);
+
+console.log(newDate.toLocaleString('defalut', {
+    weekday:"long"
+}))
