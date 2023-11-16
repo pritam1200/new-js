9c51caf41c9b86b290897c95a78c739dbfd17d39
Pritam Gharami <pritamgharami1200@gmail.com>
2023-11-16 09:49:19 +0000

    Math and Num

--- js_basics/07_nums_and_mathd.js
+++ js_basics/07_nums_and_mathd.js
@@ -22,21 +22,22 @@ const hundreds = 100000
 
 
 
-console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.5));
-// console.log(Math.round(4.5));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
-// console.log(Math.min(4,3,5,7));
-// console.log(Math.max(4,3,5,7));
+// console.log(Math);
+// console.log(Math.abs(-4)); its make the vlaue positive
+// console.log(Math.round(4.5));  do the round of value
+// console.log(Math.ceil(4.2)); always chose the heigher round off value
+// console.log(Math.floor(4.9));  always chose the lower rouned off value
+// console.log(Math.min(4,3,5,7)); it's chose the lowest value among many vlaues
+// console.log(Math.max(4,3,5,7)); it's chose the heighest value among many vlaues
 
-console.log(Math.random());
-console.log((Math.random()*10)+1);
-console.log(Math.floor(Math.random()*10)+1);
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
 
 const min = 10
 const max =20
 
 
 console.log(Math.floor(Math.random() * (max - min + 1))+min);
+
+
